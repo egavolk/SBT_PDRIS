b13d719d3e7874c9c22cd1f8709179cd98708e9a
egavolk <ega.volk@mail.ru>
2020-12-20 15:18:48 +0300

    Sonar dependencies

---
+++ Task-3/src/test/java/edu/phystech/pdris/hw/UtilTest.java
@@ -0,0 +1,12 @@
+package edu.phystech.pdris.hw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+    @Test
+    void getStringDateForPattern() {
+    }
+}
